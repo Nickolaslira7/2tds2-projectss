db23e3bdd8de0033920c71616930d66d46ed5cf9
Nickolas lira7 <125027693+Nickolaslira7@users.noreply.github.com>
2023-08-18 14:09:14 -0300

    feat: create findcategory function

--- assets/js/script.category- product.js
+++ assets/js/script.category- product.js
@@ -15,6 +15,23 @@ class Product {
     }
 }
 
+class ProductServirce{
+    constructor(){
+        this.products = [];
+        this.nextProductId = [];
+    }
+
+    addProduct(name, price, category){
+        const id = this.nextProductId;
+        this.nextProductId++;
+
+        const product = new Product(id, name, price, category);
+
+        this.products.push(product);
+        category.products.push(product);
+    }
+}
+
 class CategoryService {
     constructor(){
         this.categories = [];
@@ -26,32 +43,40 @@ const id = this.nextCategoryId;
 this.nextCategoryId ++;
 
 const category = new Category(id, name);
-this.categories.push(category)
+this.categories.push(category);
     }
 
+    getCategoriaById(id){
+        return this.categories.find((category) => category.id === id);
     }
-    class ProductService{
-        constructor(){
-            this.products= [];
-            this.nextCategoryId=1;
-        }
-        addProduct(name, price, category){
-            const id = this.nextProductId;
-            this.nextProductId++;
-
-            const Product = new Product(id, name, price, category);
-
-            this.products. push(product);
-        }
 }
 
-
 const categoriesList = new CategoryService();
+const productList = new ProductServirce();
+const category = new Category();
 
 function createCategory() {
-    const categoryName = "Candies";
+    const categoryName1 = "Candies";
+    const categoryName2 = "Shoes";
+    const categoryName3 = "Books";
 
-    categoriesList.addCategory(categoryName);
+    categoriesList.addCategory(categoryName1);
+    categoriesList.addCategory(categoryName2);
+    categoriesList.addCategory(categoryName3);
 
     console.log(categoriesList.categories);
+}
+
+function createProduct() {
+    const productName = "Choco";
+    const productPrice = 0.50;
+    const productCategory = categoriesList.categories[0];
+
+    productList.addProduct(productName, productPrice, productCategory);
+
+    console.log(productList.products);
+}
+function findCategory(id){
+    const category = categoriesList.getCategoriaById(id);
+    console.log(category);
 }
