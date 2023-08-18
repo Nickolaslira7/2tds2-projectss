a1add31385c4fe42556ae66bbe21fb6c919a66c8
Nickolas lira7 <125027693+Nickolaslira7@users.noreply.github.com>
2023-08-18 10:38:14 -0300

    feat create classes

--- assets/js/script.category- product.js
+++ assets/js/script.category- product.js
@@ -1,39 +1,57 @@
-// cada Categoria possui vários produtos
-// cada Produto possui uma categoria
-
-class Category {
-    constructor(id, name){
-      this.id = id;
-      this.name = name;
-      this.products= [];
+class Category{
+    constructor(id, name) {
+this.id = id; 
+this.name = name;
+this.products = [];
     }
 }
 
-class Product{
+class Product {
     constructor(id, name, price, category){
-        this.id= id;
-        this.name= name;
-        this.price= price;
-        this.category= category;
+        this.id = id;
+        this.name  = name;
+        this.price = price;
+        this.category = category;
     }
 }
 
-class CategoryService{
+class CategoryService {
     constructor(){
-        this.categorias = [];
-        this. nextCategoryId = 1;
+        this.categories = [];
+        this.nextCategoryId = 1;
     }
+
     addCategory(name){
-        const id = this.nextCategoryId;
-        this.nextCategoryId++;
-        
-        const category = new Category(id, name);
-        this.categories.push(category);
+const id = this.nextCategoryId;
+this.nextCategoryId ++;
+
+const category = new Category(id, name);
+this.categories.push(category)
     }
-     
 
+    }
+    class ProductService{
+        constructor(){
+            this.products= [];
+            this.nextCategoryId=1;
+        }
+        addProduct(name, price, category){
+            const id = this.nextProductId;
+            this.nextProductId++;
+
+            const Product = new Product(id, name, price, category);
+
+            this.products. push(product);
+        }
 }
- const categoriesList = new CategoryService();
-function createCategory() {
 
+
+const categoriesList = new CategoryService();
+
+function createCategory() {
+    const categoryName = "Candies";
+
+    categoriesList.addCategory(categoryName);
+
+    console.log(categoriesList.categories);
 }
